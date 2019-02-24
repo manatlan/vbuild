b1cfec95ac3976a1c6199275c38a482562a81e3c
manatlan <manatlan@gmail.com>
2019-02-24 19:15:26 +0100

    online tests are back

--- tests/test_jsminOnline.py
+++ tests/test_jsminOnline.py
@@ -2,30 +2,31 @@
 import vbuild
 import pytest
 
-# ~ def test_bad():
-# ~ s="""
-# ~ kk{{_=*jùhgj;://\\}$bc(.[hhh]
-# ~ """
-# ~ with pytest.raises(vbuild.VBuildException): # vbuild.VBuildException: minimize error: [{'type': 'JSC_PARSE_ERROR', 'file': 'Input_0', 'lineno': 2, 'charno': 10, 'error': 'Parse error. Semi-colon expected', 'line': '        kk{{_=*jùhgj;://\\}$bc(.[hhh]'}]
-# ~ vbuild.jsminOnline(s)
+def test_bad():
+    s="""
+kk{{_=*jùhgj;://\\}$bc(.[hhh]
+"""
+    with pytest.raises(vbuild.VBuildException): # vbuild.VBuildException: minimize error: [{'type': 'JSC_PARSE_ERROR', 'file': 'Input_0', 'lineno': 2, 'charno': 10, 'error': 'Parse error. Semi-colon expected', 'line': '        kk{{_=*jùhgj;://\\}$bc(.[hhh]'}]
+        vbuild.jsminOnline(s)
 
-# ~ def test_min():
-# ~ s="""
-# ~ async function  jo(...a) {
-# ~ var f=(...a) => {let b=`hello`}
-# ~ }
-# ~ """
-# ~ x=vbuild.jsminOnline(s)
-# ~ assert "$jscomp" in x
+def test_min():
+    s="""
+async function  jo(...a) {
+var f=(...a) => {let b=`hello`}
+}
+"""
+    x=vbuild.jsminOnline(s)
+    assert "$jscomp" in x
 
-# ~ def test_pycomp_onlineClosurable():
-# ~ """ Ensure python component produce a JS which is closure's online ready !"""
-# ~ cp="""<template><div>yo</div></template><script>class Component: pass</script>"""
-# ~ try:
-# ~ default=vbuild.fullPyComp
-# ~ vbuild.fullPyComp=False
-# ~ r=vbuild.VBuild("c.vue",cp)
-# ~ x=vbuild.jsminOnline(r.script)
-# ~ assert x
-# ~ finally:
-# ~ vbuild.fullPyComp=default
+
+def test_pycomp_onlineClosurable():
+    """ Ensure python component produce a JS which is closure's online ready !"""
+    cp="""<template><div>yo</div></template><script>class Component: pass</script>"""
+    try:
+        default=vbuild.fullPyComp
+        vbuild.fullPyComp=False
+        r=vbuild.VBuild("c.vue",cp)
+        x=vbuild.jsminOnline(r.script)
+        assert x
+    finally:
+        vbuild.fullPyComp=default
